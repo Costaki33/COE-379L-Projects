fd9bc8b4022e8782d7d8dea43281f38602396666
nikhil-sharma710 <98418670+nikhil-sharma710@users.noreply.github.com>
2024-04-11 11:41:25 -0500

    Update api.py

--- project3/api.py
+++ project3/api.py
@@ -22,7 +22,7 @@ def preprocess_input(im):
    # convert to a numpy array
    d = np.array(im)
    # then add an extra dimension
-   return d.reshape(1, 128, 128)
+   return d.reshape(1, 128, 128, 3)
 
 @app.route('/model/predict', methods=['POST'])
 def classify_image():
